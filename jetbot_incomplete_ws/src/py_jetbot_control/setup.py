e3b75f259f38c3e7267900cb6a86e099c6c5b520
Mike Likes Robots <mikelikesrobots@outlook.com>
2023-11-15 12:16:10 -0800

    Change maintainer name and email

--- jetbot_incomplete_ws/src/py_jetbot_control/setup.py
+++ jetbot_incomplete_ws/src/py_jetbot_control/setup.py
@@ -14,7 +14,7 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='jetbot',
+    maintainer='Mike Likes Robots',
     maintainer_email='mikelikesrobots@outlook.com',
     description='Teleoperation of Waveshare Jetbot AI Kit with keyboard or gamepad',
     license='MIT-0',
